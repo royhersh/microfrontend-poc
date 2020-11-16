1ce3bcacae43d30082ed444818a80a93a70ac0ee
Roy Hershkovitz <roy.hershkovitz@autodesk.com>
2020-11-16 22:49:59 +0200

    sharing faker package between remotes

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
       filename: 'remoteEntry.js',
       exposes: {
         './CartShow': './src/index',
-      }
+      },
+      shared: ['faker'],
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
