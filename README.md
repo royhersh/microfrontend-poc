62493528d0e08430f39b776dc02e239f52a3305a
Roy Hershkovitz <roy.hershkovitz@autodesk.com>
2020-11-23 22:55:50 +0200

    implement mount fn

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ The purpose of this project is to play around with micro frontend using webpack'
 
 ## The Gist
 
+### Container
+>webpack.config.js
+```javascript
+    new ModuleFederationPlugin({
+      name: 'products',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './ProductsIndex': './src/bootstrap',
+      },
+      shared: ['faker'],
+    }),
+```
--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,8 @@
-import 'products/ProductsIndex';
-import 'cart/CartShow';
+import { mount as mountProduct } from 'products/ProductsIndex';
+import { mount as mountCart } from 'cart/CartShow';
 
+const productsEl = document.querySelector('#my-products');
+const cartEl = document.querySelector('#my-cart');
+mountProduct(productsEl);
+mountCart(cartEl);
 console.log('container');
