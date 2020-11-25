213692c4dbee21dcdc1a6a2e8d7f2e70a14729e1
Roy Hershkovitz <roy.hershkovitz@autodesk.com>
2020-11-25 23:16:08 +0200

    p2 - basic module federation between marketing and container (no mount yet)

--- project2/packages/marketing/config/webpack.dev.js
+++ project2/packages/marketing/config/webpack.dev.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const commonConfig = require('./webpack.common');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 const devConfig = {
   mode:'development',
@@ -13,7 +14,14 @@ const devConfig = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html'
-    })
+    }),
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap'
+      },
+    }),
   ]
 };
 
